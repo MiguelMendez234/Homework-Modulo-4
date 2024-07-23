16c5b0d7acb4dcb756c442ca87a03e2d75e11dcf
MiguelMendez234 <miguelmendeza1215@gmail.com>
2024-07-23 16:24:39 -0500

    Se sube solucion de la tarea 1

---
+++ Homework-Clase-1/main.js
@@ -0,0 +1,14 @@
+let div = document.getElementById('div')
+div.innerHTML= '<h1> HOLA MUNDO  </h1>'
+div.style.display = 'flex'
+div.style.alignItems = 'center'
+div.style.justifyContent = 'center'
+div.style.heigh= '400px'
+div.style.width= '500px'
+div.style.border= 'solid, white, 2px'
+
+let body = document.getElementById('body')
+body.style.background= 'lightBlue'
+body.style.display = 'flex' 
+body.style.justifyContent= 'center'
+
